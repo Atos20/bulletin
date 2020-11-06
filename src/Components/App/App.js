16b08c6abc1ed0eb8e2ab0b6bd68795d18912c58
Orlando Murcio <ato.murcio@gmail.com>
2020-11-06 00:45:46 -0700

    Populate state keys

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,17 +1,32 @@
-import './App.scss';
 import React, { Component } from 'react';
+import './App.scss';
+import { getTopStories } from '../../apiCalls'
+import allNewsCategories from '../../data/data'
 
 export class App extends Component {
   constructor(){
     super()
-
+    
     this.state = {
-
+      newsData: {},
+      allNewsCategories: allNewsCategories,
+      error: ''
     }
   }
+  
+  componentDidMount = () => {
+    allNewsCategories.forEach(category => {
+      this.setState(prevState => ({
+        newsData: {...prevState.newsData, [category]: 'something'}
+      }))
+    })
+  }
+
+  
+
 
   render() {
-
+    
     return (
       <div className="App">
         <h1 className="hola">hola</h1>
