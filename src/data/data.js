f9eba1015a6f8271ddd5a01addb44b4ad99cfd72
Orlando Murcio <ato.murcio@gmail.com>
2020-11-05 22:48:31 -0700

    Create initial data

---
+++ src/data/data.js
@@ -0,0 +1,30 @@
+const allNewsCategories = [
+    'arts', 
+    'automobiles', 
+    'books',
+    'business',
+    'fashion',
+    'food', 
+    'health', 
+    'home', 
+    'insider', 
+    'magazine', 
+    'movies', 
+    'nyregion', 
+    'obituaries', 
+    'opinion', 
+    'politics', 
+    'realestate', 
+    'science', 
+    'sports', 
+    'sundayreview', 
+    'technology', 
+    'theater', 
+    't-magazine', 
+    'travel', 
+    'upshot', 
+    'us', 
+    'world'
+];
+
+export default allNewsCategories
