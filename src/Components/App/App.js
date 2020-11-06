a3d1f2e7e5098df700a097659d80be1fe4776821
Orlando Murcio <ato.murcio@gmail.com>
2020-11-05 21:32:33 -0700

    Remove initial set up, create component files and install dependencies

---
+++ src/Components/App/App.js
@@ -0,0 +1,23 @@
+import './App.scss';
+import React, { Component } from 'react';
+
+export class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <h1 className="hola">hola</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
