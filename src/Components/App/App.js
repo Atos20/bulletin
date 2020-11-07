50e778090ebdef78b409a99f9526214a4e8fd48c
Orlando Murcio <ato.murcio@gmail.com>
2020-11-06 17:42:33 -0700

    Display current news category

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,7 +3,7 @@ import './App.scss';
 import { getTopStories } from '../../apiCalls'
 import allNewsCategories from '../../data/data'
 import { HomePage } from '../HomePage/HomePage'
-import { Route, Router, Switch } from 'react-router-dom';
+// import { Route, Router, Switch } from 'react-router-dom';
 export class App extends Component {
   constructor(){
     super()
@@ -11,25 +11,35 @@ export class App extends Component {
     this.state = {
       newsData: {},
       allNewsCategories: allNewsCategories,
-      error: ''
+      selectedCategories: [],
+      currentCategory: {},
+      error: '',
     }
   }
-  
-  // componentDidMount = () => {
-  //   this.populateCategories()
-  //   this.requestData()
-  // }
 
-  populateCategories = () => {
-    allNewsCategories.forEach(category => {
-      this.setState(prevState => ({
-        newsData: {
-          ...prevState.newsData, 
-          [category]: {}
-        }
-      }))
-    })
-    
+  componentDidMount =  async () => {
+    await this.requestData()
+    await this.generateRandomCategory()
+  }
+  
+  generateRandomCategory = async () => {
+    const randomCategory = this.state.allNewsCategories[Math.floor(Math.random() * allNewsCategories.length)];
+    console.log(this.state.newsData)
+  }
+
+  displayChosenCategory = async (category = 'arts') => {
+    const chosenOne =  await this.state.newsData[category]
+    console.log(chosenOne)
+    const newData = Object.keys(chosenOne).reduce((data) =>{
+      data.section = chosenOne.section
+      data.topStories = chosenOne.results
+      data.last_updated = chosenOne.last_updated
+      return data
+    }, {})
+    console.log(newData)
+    this.setState(prevState => ({
+      currentCategory: {...prevState.currentCategory =  newData}
+    }))
   }
 
   requestData = async() => {
@@ -49,18 +59,27 @@ export class App extends Component {
     }
   }
 
+  selectCategory = (event) => {
+    console.log(event)
+    const category = event.target.id
+    if (!this.state.selectedCategories.includes(category)){
+      this.setState({selectedCategories: [...this.state.selectedCategories, category]});
+    }
+    this.displayChosenCategory(category)
+  }
+
   render() {
 
     return (
       <div className="App">
 
-      {/* <Router
-      exact
-      path='/home'> */}
+
         <HomePage 
           newsData={this.state.newsData}
+          selectCategory={this.selectCategory}
+          currentCategory={this.state.currentCategory}
         />
-      {/* </Router> */}
+
 
       </div>
     )
