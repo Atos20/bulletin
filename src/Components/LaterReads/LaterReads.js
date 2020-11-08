0a5c8874e0a6dd82cb48de81eeb1aaa8761ba865
Orlando Murcio <ato.murcio@gmail.com>
2020-11-08 07:00:18 -0700

    Create compoenet that displays saved stories

--- src/Components/LaterReads/LaterReads.js
+++ src/Components/LaterReads/LaterReads.js
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from 'react';
+import './LaterReads.scss';
+import moment from 'moment';
 
-export const LaterReads = () =>{
+export const LaterReads = (props) =>{
+    console.log(props)
+    const injectUserSaves = () => {
+        if(props.laterReadings.length === 0){
+            return <h1 className="sorry">Sorry but there are no readings</h1>
+        }
+        
+       return props.laterReadings.map(story => {
+           return(
+            <article className="story-container">
+                <div className="article-title-container">
+
+                <h2 className="article-title">{story.title}</h2>
+                </div>
+                
+                <div className="image-container">
+                  {/* <img id='' className="article-img" src={story.multimedia[0].url} alt=''/> */}
+                  <img id={story.created_date} className="article-img" src={ story.multimedia ? story.multimedia[0].url :'https://upload.wikimedia.org/wikipedia/en/d/d6/Image_coming_soon.png'} alt={"image no available"}/>
+                </div>
+
+                <div className="additional-info">
+                    <p className="info published_date">Published date {moment(story.published_date).format('LLLL')}</p>
+                    <p className="info updated_date">Updated date {moment(story.updated_date).format('LLLL')}</p>
+                </div>
+                <div className="abstract-container">
+                    <p className="abstract-content">{story.abstract}</p>
+                </div>
+                <div className="by-line-container">
+                    <p className="by-line-content">{story.byline}</p>
+                </div>
+                <div className="add-icon">
+                    <h6 className="listen-here">liste-here</h6>
+                    <a href={story.url} className="link"></a>
+                </div>
+
+                <div
+                    className="delete-icon">
+                    <i 
+                    id={`${story.newsType}#${story.created_date}`}
+                    onClick={(event) => props.deleteSavedReading(event)}
+                    className="fas fa-times-circle"></i>
+                   
+                </div>
+
+
+            </article>
+           )
+       })
+    }
     return (
         <div className="all-read-container">
-            <h1>hola</h1>
+            <div className="inner-container">
+                <h1 className="user-title">Your reads</h1>
+                {injectUserSaves()}
+            </div>
         </div>
     )
 }
