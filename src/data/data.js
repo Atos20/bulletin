d087d8b2e89292b742c44fb28b960dbba9fb37ee
Orlando Murcio <ato.murcio@gmail.com>
2020-11-08 19:44:13 -0700

    Arrange elements for testing purposes

--- src/data/data.js
+++ src/data/data.js
@@ -27,4 +27,48 @@ const allNewsCategories = [
     // 'world'
 ];
 
+   // const politicsCategory =  screen.getByText('politics')
+    // const sportsCategory =  screen.getByText('sports')
+    // const moviesCategory =  screen.getByText('movies')
+    // const fashionCategory = screen.getByText('fashion')
+    // const foodCategory = screen.getByText('food')
+    // const technologyCategory = screen.getByText('technology')
+    // const travelCategory = screen.getByText('travel')
+    // const worldCategory = screen.getByText('world')
+    // const businessCategory = screen.getByText('business'); 
+    // const healthCategory = screen.getByText('health');
+    // const insiderCategory = screen.getByText('insider');
+    // const magazineCategory = screen.getByText('magazine');
+    // const nyregionCategory= screen.getByText('nyregion');
+    // const obituariesCategory = screen.getByText('obituaries');
+    // const opinionCategory = screen.getByText('opinion');
+    // const realstateCategory = screen.getByText('realestate');
+    // const scienceCategory = screen.getByText('science');
+    // const sundayreviewCategory = screen.getByText('sundayreview');
+    // const theaterCategory = screen.getByText('theater');
+    // const upshotCategory = screen.getByText('upshot');
+    // const usCategory = screen.getByText('us');
+
+    // expect(moviesCategory).toBeInTheDocument()
+    // expect(sportsCategory).toBeInTheDocument()
+    // expect(politicsCategory).toBeInTheDocument()
+    // expect(fashionCategory).toBeInTheDocument();
+    // expect(foodCategory).toBeInTheDocument();
+    // expect(technologyCategory).toBeInTheDocument();
+    // expect(travelCategory).toBeInTheDocument();
+    // expect(worldCategory).toBeInTheDocument();
+    // expect(businessCategory).toBeInTheDocument();  
+    // expect(healthCategory).toBeInTheDocument();  
+    // expect(insiderCategory).toBeInTheDocument();  
+    // expect(magazineCategory).toBeInTheDocument();  
+    // expect(nyregionCategory).toBeInTheDocument();  
+    // expect(obituariesCategory).toBeInTheDocument();  
+    // expect(opinionCategory).toBeInTheDocument();  
+    // expect(realstateCategory).toBeInTheDocument();  
+    // expect(scienceCategory).toBeInTheDocument();  
+    // expect(sundayreviewCategory).toBeInTheDocument();
+    // expect(theaterCategory).toBeInTheDocument();
+    // expect(upshotCategory).toBeInTheDocument();
+    // expect(usCategory).toBeInTheDocument();
+
 export default allNewsCategories
